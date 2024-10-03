9e5a47484290e0acb26c915955e3cd29fb1512ca
hkhoi1589 <hkhoi1589@gmail.com>
2024-10-03 15:33:59 +0700

    change email

--- bot.js
+++ bot.js
@@ -1,19 +1,20 @@
 import { launch } from 'puppeteer';
 
+// find connect running instance browser?
+let browser;
 (async () => {
     try {
-        const browser = await launch({
-            headless: false,
-            defaultViewport: null,
-            args:['--start-maximized']
-        });
+        if(!browser) 
+            browser = await launch({
+                headless: false,
+                defaultViewport: null,
+                args:['--start-maximized'],
+            });
         const page = await browser.newPage();
         
         // Navigate the page to a URL.
-        await page.goto('http://localhost:8000/register/');
-
-        // Set screen size.
-        await page.setViewport({width: 0, height: 0});
+        await page.goto('https://ttt2dev.tradetotravel.com/register/');
+        // await page.goto(args[1]) // Go to the url the user specified
 
         // Type into first name.
         await page.locator('#id_user-first_name').fill('Huynh');
@@ -22,7 +23,7 @@ import { launch } from 'puppeteer';
         await page.locator('#id_user-last_name').fill('Khoi');
 
         // Type into email.
-        await page.locator('#id_user-email').fill('test22@gmail.com');
+        await page.locator('#id_user-email').fill('test10@gmail.com');
 
         // Type into password.
         await page.locator('#id_user-password1').fill('abc12345');
